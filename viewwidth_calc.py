eeb9bde8628e539ce8bba64877b9af6087ecadc9
mhgarry <mhgarry92@gmail.com>
2024-02-10 00:10:55 -0500

    init

---
+++ viewwidth_calc.py
@@ -0,0 +1,8 @@
+# ask user if they want to convert pixels to vw or vh units 
+# ask user for the number of pixels
+# ask user for the viewport width or height
+# calculate the vw or vh units
+# print the result
+# ask user if they want to convert more pixels
+# if yes, repeat the process
+# if no, end the program
